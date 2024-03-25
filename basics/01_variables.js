afd031a927bda5ad25c4fa397efa8834e7b3119b
Daniyal74 <164576114+Daniyal74@users.noreply.github.com>
2024-03-25 07:39:31 +0000

    container config done

---
+++ basics/01_variables.js
@@ -0,0 +1,20 @@
+//Inorder to store information of any user, we need some memory space to store information and the optimize way of doing this is variables and constant.
+//For constant, once the value is declared then it can't be changed.
+//For variables, values are declared and can be changed.
+const accountId=144553
+let accountEmail="daniyalsyed074@gmail.com"
+var accountPassword="12345"
+accountCity="Karachi"
+
+//accountId = 233 (declared value of constant can't be changed)
+console.log(accountId);
+
+accountEmail="daniyal@yahoo.com"
+accountPassword="21478"
+accountCity="Lahore"
+
+/*
+Prefer not to use var because of an issue of block scope and functional scope.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
