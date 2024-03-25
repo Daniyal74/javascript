262106b085038f03db776ac73128c55cea368b40
Daniyal74 <164576114+Daniyal74@users.noreply.github.com>
2024-03-25 11:15:02 +0000

    conatainer config done

--- basics/02_datatypes.js
+++ basics/02_datatypes.js
@@ -2,3 +2,15 @@
 
 // alert(3+3); // we are using nodejs, not browser.
 
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean = true/false
+// null = standalone value
+// undefined = value doesn't defined
+// symbol = uniqueness
+// object
+
+console.log(typeof "Daniyal")
+
