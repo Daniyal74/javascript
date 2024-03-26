565fab10ed40401116c8f00ab67db7f974170bc6
Daniyal74 <164576114+Daniyal74@users.noreply.github.com>
2024-03-26 19:47:41 +0000

    container config done

---
+++ basics/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// for comparision, we have to make sure that the two value which should be compared belongs to same datatypes.
+
+// console.log("2" >1);
+// console.log("02"<1)
+
+// console.log(null<0)
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0) // comparison convert the null into number then gives answer after comparison.
+
+//  '===' not only chechk the equality of the inout values but also check the datatypes of the declared values.
+// '==' only check the equality of the values and doesn't check the datatypes of the declared values.
+
+console.log("2"==2) // Doesn't compare datatype,just procide the answer only.
+console.log("2"===2) //perform strict checking means also check the dataypes of the declared values.
+
+
+
+
+
+
+
+
+
