bf065b335609fa4b682d60ccef8bd19136289c21
Daniyal74 <164576114+Daniyal74@users.noreply.github.com>
2024-03-26 06:43:35 +0000

    container config done

---
+++ basics/03_conversionoperations.js
@@ -0,0 +1,26 @@
+"user strict"
+let score= null
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number (score) //This decleration accept any type of value in the number type.
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // Give NaN (Not a Number) as output
+
+// If we provide value "null" to a variable score then it will be converted into number and provide zero as an output.
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "daniyal"
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "daniyal" => true
+
+
+
+
