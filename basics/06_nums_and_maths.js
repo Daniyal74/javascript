5b753b3a77658193e0faf44ec7c7e7d66b1ee88c
Daniyal74 <164576114+Daniyal74@users.noreply.github.com>
2024-04-10 20:38:03 +0000

    container config done

--- basics/06_nums_and_maths.js
+++ basics/06_nums_and_maths.js
@@ -1,7 +1,7 @@
 "user strict"
 
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(300)
 // console.log(balance);
@@ -20,8 +20,38 @@ const otherNumber = 124.678
 // console.log(otherNumber.toPrecision(2));
 
 const hundreds = 1000000000
-console.log(hundreds.toLocaleString());
-console.log(hundreds.toLocaleString("en-IN"));
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// *************************** MATHS **********************************
+
+// console.log(Math);
+
+// console.log(Math.abs(-4.7));
+// console.log(Math.round(4.2));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(5.4));
+// console.log(Math.floor(5.7));
+// console.log(Math.exp(10));
+
+// console.log(Math.min(45,67,87,34,23));
+// console.log(Math.max(45,67,87,34,23));
+
+// console.log(Math.random());  // always gives value between 0 and 1.
+// console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.random());
+console.log(Math.random() * (max - min + 1));
+console.log(Math.floor(Math.random() * (max - min + 1)));
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
+
+
+
 
 
 
