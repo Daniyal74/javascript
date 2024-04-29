2e15dc32f53736d6cd6a1da84c9e898089e054a4
Daniyal74 <164576114+Daniyal74@users.noreply.github.com>
2024-04-29 08:30:34 +0000

    container config done

--- 03_basics/arrow.js
+++ 03_basics/arrow.js
@@ -65,8 +65,101 @@ inorder to remove the global scope pollution and variables we use IIFE. */
     console.log(`DB Connected Two ${name}.`);
 })("Daniyal")
 
+/* Javascript Execution Context = Means how JS runs a file.
+- For running JS will do it in two phases. Firstly, Global EC is formed and can be referred by "this"
+- Global EC of browser is different from nodeJS environment. The value of Global EC of this is
+"Window Object".
+- Execution is being done on thread, Javascript is single threaded and everything in JS is being
+done through process.
+- Function/Functional Execution Context.
+- Eval Execution context (MangoDB) = Itself a property of global object. */
 
+/* {Javascript code} is being run in two phases.
+- Memory Creation Phase/ Creation Phase = Location is allocated to the variables and declarations.
+- Execution Phase = executes the code. */
 
+// ****************** EXAMPLE *******************************************
 
+let val1 = 10
+let val2 = 5
+function add_Num(num1, num2) {
+    let total = num1 + num2
+    return total
+}
 
+let result1 = add_Num(val1, val2)
+let result2 = add_Num(10, 2)
+console.log(result1);
+console.log(result2);
+
+/* 1. Code is run by Global Execution/ Environment and allocated in "this".
+
+2. Memory Phase = all variable are collected and place in memory.
+- val1 -> undefined
+- val2 -> undefined
+- add_Num -> defination of function
+- result1 -> undefined
+- result2 -> undefined
+
+3. Execution Phase:
+- val1 <- 10
+- val2 <- 5
+- add_Num = function is defined and a specified execution context is formed for add_Num
+
+                            (New variable Environment + Execution thread)
+                            
+                            * Will be deleted after execution.
+
+Everytime when function is executed then a new box is created named New Execution Context contains
+a separate environment box for variables(Memory Phase) and a separate execution thread(Execution Phase).
+- For function add_Num:
+ Memory Creation:
+- val1 -> undefined
+- val2 -> undefined
+- total -> undefined
+
+Execution Context:
+Processing and execution is being done in this phase.
+
+- num1 <- 10
+- num2 <- 5
+- total <- 15 (return to the Global Execution Context).
+
+After the execution of function the box will be deleted and we will revert back to step (3).
+
+- result1 <- 15
+
+For result2, again a separate block is formed (New Variable Environment + Execution Thread).
+
+Memory Phase:
+- val1 -> undefined
+- val2 -> undefined
+- total -> undefined
+
+Execution Context:
+Processing and execution is being done in this phase.
+
+- num1 <- 10
+- num2 <- 2
+- total <- 12 (return to the Global Execution Context).
+After the execution of function the box will be deleted and we will revert back to step (3).
+
+result2 <- 12  */
+
+//  ************************** Call Stack *******************************
+
+/*
+
+                                    |                        |     
+                                    |                        |
+                                    |                        |
+                                    |                        |
+                                    |        three()         |
+                                    |        two()           |
+                                    |        one()           |
+                                    |      Global EC         |
+                                    |________________________|
+
+LIFO (Last In First Out) technique is being used for execution the last function will executes
+first and then leave the stack.
 
