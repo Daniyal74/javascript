495e7dbe3451385bf06ddbeeb7394cbd195e8f51
Daniyal74 <164576114+Daniyal74@users.noreply.github.com>
2024-05-03 05:01:22 +0000

    container config done

--- 04_iterations/loops.js
+++ 04_iterations/loops.js
@@ -72,6 +72,7 @@ while (i <= 10) {
 }
 */
 
+/*
 let myArray = ["Antman", "Ironman", "Hulk"]
 
 let arr = 0
@@ -79,6 +80,15 @@ while (arr < myArray.length) {
     console.log(`Value is ${myArray[arr]}.`);
     arr++
 }
+*/
+
+let score = 12
+do {
+    console.log(`Score is ${score}.`);
+    score++
+} while (score <= 10);
+
+
 
 
 
